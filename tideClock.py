68afecc25476fe303cc1acb0af70772d672a554a
pro-use <robertprouse@gmail.com>
2026-02-26 11:58:45 +0000

    zero index max steps

--- tideClock.py
+++ tideClock.py
@@ -16,7 +16,7 @@ class Stepper:
     def __init__(self, motor_pin, sensor_pin):
         self.STEP = OutputDevice(motor_pin)
         self.sensor = Button(sensor_pin)
-        self.MOTOR_STEPS = 200
+        self.MOTOR_STEPS = 199
         self.MICRO = 8
         self.MICROSTEP = 1 / self.MICRO
         self.STEPS = self.MOTOR_STEPS * self.MICRO
