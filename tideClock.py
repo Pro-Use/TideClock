3534c2d80c370a17452bf1c89f499228b5ba7b31
pro-use <robertprouse@gmail.com>
2026-03-03 14:45:44 +0000

    zero check can be equal to range

--- tideClock.py
+++ tideClock.py
@@ -94,7 +94,7 @@ class Stepper:
     def earlyZeroCheck(self):
         if self.sensor.is_pressed:
             # Check both ranges either side of zero
-            if not (self.triggered_start < self.position < self.MOTOR_STEPS) and not (0 <= self.position < self.triggered_stop):
+            if not (self.triggered_start <= self.position <= self.MOTOR_STEPS) and not (0 <= self.position <= self.triggered_stop):
                 print("Warning: not zero target but sensor is active! Position: %.2f start: %.2f stop: %.2f" % (self.position, self.triggered_start, self.triggered_stop))
                 self.zeroed = False
                 self.zero()
