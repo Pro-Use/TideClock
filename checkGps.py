7ea755d881c0486c17ee118cd32168e1bcc0f317
pro-use <robertprouse@gmail.com>
2026-06-23 15:31:21 +0100

    add gps check script

---
+++ checkGps.py
@@ -0,0 +1,17 @@
+import pps_tools
+from gpiozero import OutputDevice
+from time import time, sleep
+
+LED = OutputDevice(22)
+ledState = False
+
+with pps_tools.PpsFile("/dev/pps0") as ppsf:
+    while True:
+        try:
+            edge = ppsf.fetch(timeout=2)
+            print(f"Edge: {edge}")
+            print(f"Time diff: {time() - edge['assert_time']}")
+            LED.toggle()
+            print(f"LED state: {LED.value}")
+        except TimeoutError:
+            print("Timeout waiting for PPS signal")
